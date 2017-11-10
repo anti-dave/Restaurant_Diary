6948abf56f6612e683fe967a64756855c67d79a4
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2017-11-10 14:29:38 -0500

    demo text in searchActivity
    finished Restaurant class and Adapter.
    and put in demo in search activity

--- RestaurantDiaryApp/app/src/main/java/com/example/android/restaurantdiary/SearchActivity.java
+++ RestaurantDiaryApp/app/src/main/java/com/example/android/restaurantdiary/SearchActivity.java
@@ -2,6 +2,9 @@ package com.example.android.restaurantdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -16,5 +19,17 @@ public class SearchActivity extends AppCompatActivity {
         *
         * user entered text can be found in ----> R.id.search_activity_user_input <---- use it as you will...
         * */
+
+        // jake says yo and demo/check that adapter works
+        ArrayList<Restaurant> locations = new ArrayList<Restaurant>();
+
+        locations.add(new Restaurant("Mario's Pizza", "pizza","123 Boston Street", "781 123 4567","4"));
+        locations.add(new Restaurant("Luigi's Pizza", "pizza","124 Boston Street", "781 123 4568","5"));
+        locations.add(new Restaurant("Clam Chowder?", "soup","5 MysteryMeat Ave", "156 545 1846","1"));
+        locations.add(new Restaurant("The House Of Ramen", "raman", "5 Ramen Road", "012 345 6789","11"));
+
+        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(this, locations);
+        ListView listView = (ListView) findViewById(R.id.restaurant_in_search_list);
+        listView.setAdapter(restaurantAdapter);
     }
 }
